8adc705163309b866c2b5a327208ac22d7b47ada
Michael Welle <michaelpwelle@gmail.com>
2023-03-23 00:05:36 -0700

    added to routes, still troubles with get singles

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -31,6 +31,12 @@ const productData = [
     stock: 22,
     category_id: 2,
   },
+  {
+    product_name: 'Business Casual Polo',
+    price: 34.99,
+    stock: 8,
+    category_id: 1,
+  },
 ];
 
 const seedProducts = () => Product.bulkCreate(productData);
